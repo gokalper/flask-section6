3e8ba1d331f98dbbbcc356858b4c5f5e5b67f4b3
Gokalp Ercilasun <gokalp_ercilasun@intuit.com>
2018-12-24 19:07:15 -0800

    master

--- resources/item.py
+++ resources/item.py
@@ -2,7 +2,7 @@ import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
-
+git
 class Item(Resource):
     TABLE_NAME = 'items'
 
